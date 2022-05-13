dcde12e379d0f3cc4634b56c203858a5aba296cc
Viola-99 <65301306+Viola-99@users.noreply.github.com>
2022-05-13 15:35:56 +0300

    Collision
    Not finished

---
+++ Assets/Scripts/tile_col.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class tile_col : MonoBehaviour
+{
+    [SerializeField] public GameObject tilesColliderObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
