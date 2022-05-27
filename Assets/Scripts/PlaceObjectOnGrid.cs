03b9d5ac0de539530dfb99dcc24ff089486b3cc9
Viola-99 <65301306+Viola-99@users.noreply.github.com>
2022-05-27 09:58:03 +0300

    Record_problems

--- Assets/Scripts/PlaceObjectOnGrid.cs
+++ Assets/Scripts/PlaceObjectOnGrid.cs
@@ -21,14 +21,14 @@ public class PlaceObjectOnGrid : MonoBehaviour
     [SerializeField] private List<Color> colors;
     [SerializeField] private List<GameObject> instancedTiles;
     [SerializeField] public GameObject prefab;
-    Dictionary<Vector3, GameObject> someDictionary;
+    Dictionary<Vector3Int, GameObject> someDictionary;
    
 
 
     void Start()
     {
        
-        someDictionary = new Dictionary<Vector3, GameObject>();
+        someDictionary = new Dictionary<Vector3Int, GameObject>();
         CreateGrid();
         //audio = GetComponent<AudioSource>();
     }
@@ -48,11 +48,12 @@ public class PlaceObjectOnGrid : MonoBehaviour
         Compare();
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            Vector3 buf = new Vector3(smth.ramka.transform.position.x, 0.01f, smth.ramka.transform.position.z);
+            Vector3Int buf =   Vector3Int.RoundToInt(new Vector3 (smth.ramka.transform.position.x, 0.015f, smth.ramka.transform.position.z));
             someDictionary[buf].GetComponent<tile_col>().tilesColliderObject.SetActive(false);
             someDictionary[buf].GetComponent<Rigidbody>().AddForce(new Vector3(0, 350, 0));
-            someDictionary[buf].transform.parent = smth.ramka.transform;
+                 someDictionary[buf].transform.parent = smth.ramka.transform;
             anth = someDictionary[buf];
+            
 
         }
     }
@@ -62,7 +63,7 @@ public class PlaceObjectOnGrid : MonoBehaviour
     {
         nodes = new Node[width, height];
         var name = 0;
-        GameObject instantiatedRamka = Instantiate(prefab, new Vector3(1, 0.02f, 1), Quaternion.identity);
+        GameObject instantiatedRamka = Instantiate(prefab, new Vector3(1, 0.002f, 1), Quaternion.identity);
         smth.ramka = instantiatedRamka;
 
         for (int i = 0; i < width; i++)
@@ -75,13 +76,14 @@ public class PlaceObjectOnGrid : MonoBehaviour
                 obj.GetComponent<Renderer>().material.color = colors[num];
                 nodes[i, j] = new Node(true, worldPosition, obj);
                 name++;
-                instancedTiles.Add(Instantiate(tile, new Vector3(i, 0.01f, j), Quaternion.identity));
+                instancedTiles.Add(Instantiate(tile, new Vector3(i, 0.015f, j), Quaternion.identity));
                 instancedTiles[num].GetComponent<Renderer>().material.color = colors[num];
                 instancedTiles[num].GetComponent<tile_col>().border = this;
-                someDictionary.Add(instancedTiles[num].transform.position, instancedTiles[num]);
+                someDictionary.Add(Vector3Int.RoundToInt(instancedTiles[num].transform.position), instancedTiles[num]);
 
                 //anth_2 = someDictionary[num];
                 num++;
+           
 
             }
         }
@@ -89,11 +91,14 @@ public class PlaceObjectOnGrid : MonoBehaviour
 
     public void Record(GameObject tile_new)
     {
-        someDictionary.Remove(tile_new.transform.position);
+
+        //someDictionary.Remove(Vector3Int.RoundToInt(tile_new.transform.position));
+
+        //someDictionary.Add(Vector3Int.RoundToInt(tile_new.transform.position), tile_new);
+        Vector3Int pos = Vector3Int.RoundToInt(tile_new.transform.position);
+        someDictionary[pos] = tile_new;
 
 
-     //   Debug.Log("Запись" + someDictionary[0;0;0]) ;
-        someDictionary.Add(tile_new.transform.position, tile_new);
 
     }
 
@@ -112,13 +117,13 @@ public class PlaceObjectOnGrid : MonoBehaviour
                     bool isRepeating = true;
                     do
                     {
-                        instancedTiles[num_2].transform.position = new Vector3(Random.Range(0, height), 0.01f, Random.Range(0, width));
-                        if (!someDictionary.ContainsKey(instancedTiles[num_2].transform.position))
+                        instancedTiles[num_2].transform.position = new Vector3(Random.Range(0, height), 0.015f, Random.Range(0, width));
+                        if (!someDictionary.ContainsKey(Vector3Int.RoundToInt(instancedTiles[num_2].transform.position)))
                         {
                             isRepeating = false;
                         }
                     } while (isRepeating);
-                    someDictionary.Add(instancedTiles[num_2].transform.position, instancedTiles[num_2]);
+                    someDictionary.Add(Vector3Int.RoundToInt(instancedTiles[num_2].transform.position), instancedTiles[num_2]);
                 }
             }
         }
@@ -131,7 +136,7 @@ public class PlaceObjectOnGrid : MonoBehaviour
         {
                    
             Color cellColor = nodes[(int)coords.x, (int)coords.z].obj.GetComponent<Renderer>().material.color;
-            Color tileColor = someDictionary[coords].GetComponent<Renderer>().material.color;
+            Color tileColor = someDictionary[Vector3Int.RoundToInt(coords)].GetComponent<Renderer>().material.color;
             if (cellColor != tileColor)
             {
                 isColorMatchesEverywhere = false;
